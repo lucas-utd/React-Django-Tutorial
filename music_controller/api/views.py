da8d676391fd1c8c885c05189b121e043bce0b40
lucas-utd <lucaschentao@gmail.com>
2020-12-27 16:04:45 -0600

    Finish tutorial 9.

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,8 +1,7 @@
 from django.db.models.query import QuerySet
+from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework import generics, status
-from rest_framework.request import Request
-from rest_framework.serializers import Serializer
 from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
 from rest_framework.views import APIView
@@ -80,3 +79,13 @@ class CreateRoomView(APIView):
                 return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
 
         return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        data = {'code': self.request.session.get('room_code')}
+        # data = {'code': 'room-code'}
+        return JsonResponse(data, status=status.HTTP_200_OK)
